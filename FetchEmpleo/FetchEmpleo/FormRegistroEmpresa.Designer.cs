d62a26d5212d434d887018305d479ed7394ab444
hulk-lian <juliantrodom@gmail.com>
2015-05-05 19:41:58 +0200

    Añadido formulario de registro de empresa con control de los datos introducidos

---
+++ FetchEmpleo/FetchEmpleo/FormRegistroEmpresa.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace RegistroEmpresa
+{
+    partial class FrnEmpresa
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txbUsuario = new System.Windows.Forms.TextBox();
+            this.txbContraseña = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbDNI = new System.Windows.Forms.TextBox();
+            this.txbDir = new System.Windows.Forms.TextBox();
+            this.txbRS = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbtelf = new System.Windows.Forms.TextBox();
+            this.txbWeb = new System.Windows.Forms.TextBox();
+            this.btnIngresarDatos = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.pnlDatos = new System.Windows.Forms.Panel();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.pnlDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(554, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulario de registro de empresas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CIF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Razon Social";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Direccion";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(36, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Nombre";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(36, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Contraseña";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(36, 76);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 17);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Usuario";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(36, 291);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 17);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Telefono";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(36, 320);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(40, 17);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Web";
+            // 
+            // txbUsuario
+            // 
+            this.txbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUsuario.Location = new System.Drawing.Point(3, 12);
+            this.txbUsuario.Name = "txbUsuario";
+            this.txbUsuario.Size = new System.Drawing.Size(135, 23);
+            this.txbUsuario.TabIndex = 21;
+            // 
+            // txbContraseña
+            // 
+            this.txbContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbContraseña.Location = new System.Drawing.Point(3, 41);
+            this.txbContraseña.Name = "txbContraseña";
+            this.txbContraseña.PasswordChar = '*';
+            this.txbContraseña.Size = new System.Drawing.Size(135, 23);
+            this.txbContraseña.TabIndex = 22;
+            this.txbContraseña.TextChanged += new System.EventHandler(this.txbContraseña_TextChanged);
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNombre.Location = new System.Drawing.Point(3, 73);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(135, 23);
+            this.txbNombre.TabIndex = 23;
+            // 
+            // txbDNI
+            // 
+            this.txbDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDNI.Location = new System.Drawing.Point(3, 101);
+            this.txbDNI.Name = "txbDNI";
+            this.txbDNI.Size = new System.Drawing.Size(135, 23);
+            this.txbDNI.TabIndex = 24;
+            // 
+            // txbDir
+            // 
+            this.txbDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDir.Location = new System.Drawing.Point(3, 128);
+            this.txbDir.Name = "txbDir";
+            this.txbDir.Size = new System.Drawing.Size(135, 23);
+            this.txbDir.TabIndex = 25;
+            // 
+            // txbRS
+            // 
+            this.txbRS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbRS.Location = new System.Drawing.Point(3, 160);
+            this.txbRS.Name = "txbRS";
+            this.txbRS.Size = new System.Drawing.Size(135, 23);
+            this.txbRS.TabIndex = 26;
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEmail.Location = new System.Drawing.Point(3, 194);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(175, 23);
+            this.txbEmail.TabIndex = 27;
+            // 
+            // txbtelf
+            // 
+            this.txbtelf.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbtelf.Location = new System.Drawing.Point(3, 224);
+            this.txbtelf.Name = "txbtelf";
+            this.txbtelf.Size = new System.Drawing.Size(135, 23);
+            this.txbtelf.TabIndex = 28;
+            // 
+            // txbWeb
+            // 
+            this.txbWeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbWeb.Location = new System.Drawing.Point(3, 253);
+            this.txbWeb.Name = "txbWeb";
+            this.txbWeb.Size = new System.Drawing.Size(201, 23);
+            this.txbWeb.TabIndex = 29;
+            // 
+            // btnIngresarDatos
+            // 
+            this.btnIngresarDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresarDatos.Location = new System.Drawing.Point(458, 76);
+            this.btnIngresarDatos.Name = "btnIngresarDatos";
+            this.btnIngresarDatos.Size = new System.Drawing.Size(128, 49);
+            this.btnIngresarDatos.TabIndex = 30;
+            this.btnIngresarDatos.Text = "Aceptar y Darse de Alta";
+            this.btnIngresarDatos.UseVisualStyleBackColor = true;
+            this.btnIngresarDatos.Click += new System.EventHandler(this.btnIngresarDatos_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(472, 140);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(101, 31);
+            this.btnLimpiar.TabIndex = 31;
+            this.btnLimpiar.Text = "Limpiar campos";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click_1);
+            // 
+            // pnlDatos
+            // 
+            this.pnlDatos.Controls.Add(this.txbUsuario);
+            this.pnlDatos.Controls.Add(this.txbContraseña);
+            this.pnlDatos.Controls.Add(this.txbNombre);
+            this.pnlDatos.Controls.Add(this.txbWeb);
+            this.pnlDatos.Controls.Add(this.txbDNI);
+            this.pnlDatos.Controls.Add(this.txbtelf);
+            this.pnlDatos.Controls.Add(this.txbDir);
+            this.pnlDatos.Controls.Add(this.txbEmail);
+            this.pnlDatos.Controls.Add(this.txbRS);
+            this.pnlDatos.Location = new System.Drawing.Point(166, 61);
+            this.pnlDatos.Name = "pnlDatos";
+            this.pnlDatos.Size = new System.Drawing.Size(222, 288);
+            this.pnlDatos.TabIndex = 32;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(472, 189);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(101, 35);
+            this.btnCancelar.TabIndex = 33;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // FrnEmpresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 382);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.pnlDatos);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnIngresarDatos);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrnEmpresa";
+            this.Text = "Dar de alta empresa";
+            this.pnlDatos.ResumeLayout(false);
+            this.pnlDatos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txbUsuario;
+        private System.Windows.Forms.TextBox txbContraseña;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox txbDNI;
+        private System.Windows.Forms.TextBox txbDir;
+        private System.Windows.Forms.TextBox txbRS;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbtelf;
+        private System.Windows.Forms.TextBox txbWeb;
+        private System.Windows.Forms.Button btnIngresarDatos;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Panel pnlDatos;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ FetchEmpleo/FetchEmpleo/FormRegistroEmpresa.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroEmpresa
+{
+    public partial class FrnEmpresa : Form
+    {
+
+        #region variables
+        string usuario;
+        string passwd;
+        string nombre;
+        string cif;
+        string dir;
+        string razonso;
+        string email;
+        string tlf;
+        string web;
+
+        #endregion
+        public FrnEmpresa()
+        {
+            InitializeComponent();
+        }
+        private void limpiarCampos()
+        {
+            foreach (TextBox item in pnlDatos.Controls)
+            {
+                item.Text = "";
+            }
+        }
+
+        private void btnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            limpiarCampos(); 
+        }
+
+        private void btnIngresarDatos_Click(object sender, EventArgs e)
+        {
+            daoEmpresa dao = new daoEmpresa();
+            if (comprobarcampos())
+            {
+                //llamada al dao.registrar<- insert into table empresa
+            }
+            else
+            {
+                MessageBox.Show("Te has dejado campos sin rellenar");
+            }
+            
+        }
+        void llenarConDatos()
+        {
+            usuario = txbUsuario.Text.Trim(); ;
+            passwd = txbContraseña.Text.Trim(); ;
+            nombre=txbNombre.Text.Trim();
+            cif = txbDNI.Text.Trim();
+            dir = txbDir.Text.Trim();
+            razonso = txbRS.Text.Trim();
+            email = txbEmail.Text.Trim();
+            tlf = txbtelf.Text.Trim();
+            web = txbWeb.Text.Trim();
+
+        }
+        bool comprobarcampos()
+        {
+                foreach (TextBox item in pnlDatos.Controls)
+                {
+                    if (item.Text == "" || item.Text == " ")
+                    {
+                        return false;
+                    }
+                }
+                return true;
+        }
+
+        private void txbContraseña_TextChanged(object sender, EventArgs e)
+        {
+            if(txbContraseña.Text != txbContraseña.Text.Trim())
+            {
+                MessageBox.Show("La contraseña no puede llevar espacios ni al comienzo ni al final");
+                txbContraseña.Text = "";
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            //
+            this.Close();
+        }
+
+     }
+}
